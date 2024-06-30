2bd8c9347a2d153feea05d35b37952d4acb5207b
adriendev15 <adriendelanaux@outlook.fr>
2024-06-30 09:30:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ async function displayPokemon() {
   
   const reponse = await fetch("https://tyradex.vercel.app/api/v1/pokemon/pikachu");
   const pokemon = await reponse.json();
-  alert("nom: " + pokemon.name.fr + "\ncatégorie: " + pokemon.category);
-  
+  alert('nom: ' + pokemon.name.fr + '\ncatégorie: ' + pokemon.category);
+  document.getElementById('pokemonPicture').src = pokemon.sprites.regular;
+  document.getElementById('pokemonPictureShiny').src = pokemon.sprites.shiny;
 }
 
