abaf4350313270b31a925533cc2b09f808063d34
adriendev15 <adriendelanaux@outlook.fr>
2024-06-30 09:14:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function displayMessage() {
   alert("Bonjour Adrien!");
 }
 
-async function afficherFilms() {
+async function displayPokemon() {
   const reponse = await fetch("https://tyradex.vercel.app/api/v1/pokemon/pikachu");
   const pokemon = await reponse.json();
   alert(pokemon.name.fr);
